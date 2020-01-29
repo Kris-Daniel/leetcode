a0b9f32836856f8064bd7956700d1ba256b1353e
krisdaniel <krisdaniel1452@gmail.com>
2020-01-29 23:28:38 +0200

    Longest Substring - Javascript - Medium - Passed

--- LongestSubstringWithoutRepeatingCharacters/LongestSubstring.js
+++ LongestSubstringWithoutRepeatingCharacters/LongestSubstring.js
@@ -1,18 +1,16 @@
 var lengthOfLongestSubstring = function (s) {
     var max = 0;
-    var symbols = [];
+    var symbols = "";
     for (var k = 0; k < s.length; k++) {
-        for (var i = k, j = 0; i < s.length; i++) {
-            if (symbols.indexOf(s[i]) == -1) {
-                max = ++j > max ? j : max;
-            } else {
-                symbols = [];
-                max = j > max ? j : max;
-                j = 0;
+        for (var i = k; i < s.length; i++) {
+            if (symbols.indexOf(s[i]) > -1) {
+                symbols = "";
+                break;
             }
-            symbols.push(s[i]);
+            symbols += s[i];
+            max = symbols.length > max ? symbols.length : max;
         }
-        symbols = [];
+        symbols = "";
     }
     return max;
 };
